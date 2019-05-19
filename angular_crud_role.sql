5c54a2d49306ee75b827020cef0d9e3db2656bc4
ujikit <fauzizaki15@gmail.com>
2019-05-20 03:07:21 +0700

    update sql product data

--- angular_crud_role.sql
+++ angular_crud_role.sql
@@ -29,7 +29,7 @@ CREATE TABLE `products` (
   `created_at_product` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at_product` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Pepsodent','3000','2019-05-17 09:09:02','2019-05-17 09:09:02'),(2,'Aqua','500','2019-05-17 09:13:51','2019-05-17 09:13:51'),(3,'Chocolatos','2500','2019-05-17 09:14:42','2019-05-17 09:14:42');
+INSERT INTO `products` VALUES (1,'Pepsodent','3000','2019-05-17 09:09:02','2019-05-17 09:09:02'),(2,'Aqua','500','2019-05-17 09:13:51','2019-05-17 09:13:51'),(3,'Chocolatos','2500','2019-05-17 09:14:42','2019-05-17 09:14:42'),(4,'Coca-Cola','2000','2019-05-17 09:29:57','2019-05-17 09:29:57');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `transactions` (
   KEY `id_product_transaction` (`id_product_transaction`),
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id_user_transaction`) REFERENCES `users` (`id`),
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`id_product_transaction`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES (5,1,1,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(6,1,2,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(7,3,1,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(8,3,2,'2019-05-17 09:15:48','2019-05-17 09:15:48');
+INSERT INTO `transactions` VALUES (5,1,1,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(6,1,2,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(7,3,1,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(8,3,2,'2019-05-17 09:15:48','2019-05-17 09:15:48'),(9,3,4,'2019-05-17 09:31:39','2019-05-17 09:31:39');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-17 16:17:53
+-- Dump completed on 2019-05-20  3:04:57
